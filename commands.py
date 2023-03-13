ca598785cd3ac314f0129efa548f54a2efad9b31
ed19e3c <ed19e3c@leeds.ac.uk>
2023-03-13 01:55:16 +0000

    commit necessary for pull

--- commands.py
+++ commands.py
@@ -1,20 +1,52 @@
 import re
+import random
+
+import discord
+
+import util
 
 async def commandSetName(self, curMessage, curMessageSplit):
 	
-	targetId = re.search(r'<@[0-9]{1,}>', curMessageSplit[1])
-	targetId = re.search(r'[0-9]{1,}', targetId.group()).group()
+	targetId = util.parseTag(curMessageSplit[1])
 
 	for curGuild in self.guilds:
-		targetUser = await curGuild.fetch_member(targetId)
-		if targetUser != None:
+		targetMember = await curGuild.fetch_member(targetId)
+		if targetMember != None:
 			break
 
-	print(f'renaming user {targetUser.name} with id {targetUser.id} to {curMessageSplit[2]}')
+	util.renameMember(targetMember, curMessageSplit[2])
 
-	await targetUser.edit(nick = curMessageSplit[2])
 	await curMessage.reply('renamed')
 
+async def commandPartyMode(self, curMessage, curMessageSplit):
+
+	def getEmojiName():
+		newName = util.getRandomEmoji()
+		while random.randint(1, 5) != 1:
+			newName += util.getRandomEmoji()
+		return newName
+
+	if len(curMessageSplit) == 1:
+		for curGuild in self.guilds:
+			for curMember in curGuild.members:
+				newName = getEmojiName()
+				try:
+					await curMember.edit(nick = newName)
+					print(f'renaming user {curMember.name} with id {curMember.id} to {newName}')
+				except discord.errors.Forbidden as e:
+					print(f'cannot rename user {curMember.name} with id {curMember.id}')
+	elif len(curMessageSplit) == 2:
+		targetId = util.parseTag(curMessageSplit[1])
+		for curGuild in self.guilds:
+			targetMember = await curGuild.fetch_member(targetId)
+			if targetMember != None:
+				break
+		newName = getEmojiName()
+		util.renameMember(targetMember, newName)
+
+	await curMessage.reply('party')
+
 commandsList = {
-	commandSetName: ['setname', 'name', 'rename']
+	commandSetName: ['setname', 'name', 'rename', 'nick', 'renick', 'nickname', 'setnick'],
+	commandPartyMode: ['partymode', 'party'],
 }
---
+++ util.py
@@ -0,0 +1,19 @@
+import random
+import re
+
+def getRandomEmoji():
+	with open('assets/emojis.txt', 'r', encoding = 'UTF-8') as emojisFile:
+		emojisFileStr = emojisFile.read()
+		return random.choice(emojisFileStr)
+
+def parseTag(tagStr):
+	targetId = re.search(r'<@[0-9]{1,}>', tagStr)
+	targetId = re.search(r'[0-9]{1,}', targetId.group()).group()
+	return targetId
+
+async def renameMember(curMember, newName):
+	try:
+		await curMember.edit(nick = newName)
+		print(f'renamed user {curMember.name} with id {curMember.id} to {newName}')
+	except discord.errors.Forbidden as e:
+		print(f'cannot rename user {curMember.name} with id {curMember.id}, forbidden')
